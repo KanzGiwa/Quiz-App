e3fc3fcac54b113d46be3ed0746924a04b8cf6af
Kanz Giwa <kanzgiwa@gmail.com>
2025-08-02 21:40:51 -0400

    Fix ESLint error - remove unused useEffect import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Quiz from './components/Quiz';
 import Result from './components/Result';
